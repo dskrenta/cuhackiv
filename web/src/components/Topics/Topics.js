a239f06c800f382768dfe89f8100723047223813
BDesBrisay <bryce@desbrisay.com>
2018-02-24 15:38:54 -0700

    added fake topic data

--- web/src/components/Topics/Topics.js
+++ web/src/components/Topics/Topics.js
@@ -2,6 +2,20 @@ import React from 'react';
 
 import Header from '../Header/Header';
 
+const sampleTopics = [
+  {
+    user: {
+      email: 'johndoes@email.com',
+      name: 'Johnathan Doeth',
+      imageUrl: 'http://services.expreso.ec/Granasa/img/',
+    },
+    title: 'Gun Control',
+    description: 'Regulate the manufacture, sale, transfer, possession, modification, or use of firearms by civilians.',
+    tags: ['Guns','Violence','Regulations','Gun Shows'],
+    createdAt: 'Feb 24th, 2018',
+  },
+]
+
 const Topics = ({ history }) => (
   <div>
     <Header history={history} />
