b281014469b8f2d5dc4c1506d7a9a9681918602a
BDesBrisay <bryce@desbrisay.com>
2018-02-25 04:22:58 -0700

    styled topid header

--- web/src/components/Topics/Topics.js
+++ web/src/components/Topics/Topics.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Card, CardMedia } from 'material-ui';
+import { Link } from 'react-router-dom';
 
 import Header from '../Header/Header';
 
@@ -62,20 +63,22 @@ const Topics = ({ history }) => (
     <div className={styles.grid}>
       {sampleTopics.map((topic, index) => (
         <div key={index} className={styles.gridItem}>
-          <Card>
-            <CardMedia>
-              <img src={topic.image} className={styles.topicImage} alt="" />
-            </CardMedia>
-            <div className={styles.topicContent}>
-              <span className={styles.topicTitle}>{topic.title}</span>
-              <span className={styles.topicDesc}>{topic.description}</span>
-              <div className={styles.topicTags}>
-                {topic.tags.slice(0,4).map((tag, index) => (
-                  <div key={index} className={styles.topicTag}>{tag}</div>
-                ))}
-              </div>
-            </div>            
-          </Card>
+          <Link to='/topic/x' style={{textDecoration: 'none'}}>
+            <Card>
+              <CardMedia>
+                <img src={topic.image} className={styles.topicImage} alt="" />
+              </CardMedia>
+              <div className={styles.topicContent}>
+                <span className={styles.topicTitle}>{topic.title}</span>
+                <span className={styles.topicDesc}>{topic.description}</span>
+                <div className={styles.topicTags}>
+                  {topic.tags.slice(0,4).map((tag, index) => (
+                    <div key={index} className={styles.topicTag}>{tag}</div>
+                  ))}
+                </div>
+              </div>            
+            </Card>
+          </Link>
         </div>
       ))}
     </div>
